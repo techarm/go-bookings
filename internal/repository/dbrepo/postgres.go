5c7b0f0e7dcc6a602b4e18bc5682a17aaff8d6c5
techarm <techarmdev@gmail.com>
2022-03-24 22:23:52 +0900

    部屋情報検索処理を追加

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -53,3 +53,64 @@ func (m postgresDBRepo) InsertRoomRestriction(rr models.RoomRestriction) error {
 
 	return nil
 }
+
+// SearchRoomAvailabilityByDates 指摘期間内に部屋の空状況を取得
+func (m postgresDBRepo) SearchRoomAvailabilityByDates(roomID int, start, end time.Time) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `select count(*) from room_restrictions where room_id = $1 and $2 < end_date and $3 < start_date`
+	row := m.DB.QueryRowContext(ctx, stmt, roomID, start, end)
+
+	var count int
+	err := row.Scan(&count)
+	if err != nil {
+		return false, err
+	}
+
+	return count == 0, nil
+}
+
+// SearchAvailabilityForAllRooms 現在全て予約可能の部屋情報を取得
+func (m postgresDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `
+		select
+			r.id ,
+			r.room_name
+		from
+			rooms r
+		where
+			r.id not in (
+				select
+					rr.room_id
+				from
+					room_restrictions rr
+				where
+					$1 < end_date
+					and $2 > start_date
+			)`
+
+	var rooms []models.Room
+	rows, err := m.DB.QueryContext(ctx, stmt, start, end)
+	if err != nil {
+		return rooms, err
+	}
+
+	for rows.Next() {
+		var room models.Room
+		err = rows.Scan(&room.ID, &room.RoomName)
+		if err != nil {
+			return rooms, err
+		}
+		rooms = append(rooms, room)
+	}
+
+	if err = rows.Err(); err != nil {
+		return rooms, err
+	}
+
+	return rooms, nil
+}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,8 +1,13 @@
 package repository
 
-import "github.com/techarm/go-bookings/internal/models"
+import (
+	"github.com/techarm/go-bookings/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(res models.RoomRestriction) error
+	SearchRoomAvailabilityByDates(roomId int, start, end time.Time) (int, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
