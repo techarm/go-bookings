5b8beae0d94297175ff2896848538d72415728c5
techarm <techarmdev@gmail.com>
2022-03-29 22:33:23 +0900

    seedデータを作成

---
+++ migrations/20220329132423_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (id, room_name,created_at,updated_at) VALUES
+(1, 'Generals'' Quarters','2022-03-23 00:00:00.000','2022-03-23 00:00:00.000'),
+(2, 'Major''s Suite','2022-03-23 00:00:00.000','2022-03-23 00:00:00.000');
---
+++ migrations/20220329132726_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (id, restriction_name,created_at,updated_at) VALUES
+(1, 'Reservation','2022-03-23 00:00:00.000','2022-03-23 00:00:00.000'),
+(2, 'Owner Block','2022-03-23 00:00:00.000','2022-03-23 00:00:00.000');
